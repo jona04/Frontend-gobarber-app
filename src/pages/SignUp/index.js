259e7761f926ecdf0239f55702467cbd8b860411
jonatasilva <jonatas.iw@gmail.com>
2020-04-19 12:09:44 -0300

    chore: adicionar Yup e Redux Saga

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+        .email('Insira um email valido')
+        .required('O email é obrigatorios'),
+    password: Yup.string()
+        .required('A senha é obrigatoria')
+        .min(8, 'A senhap recisa ter no minimo 8 caracteres'),
+});
 
 export default function SignUp() {
     function handleSubmit(data) {
@@ -12,7 +23,7 @@ export default function SignUp() {
         <>
             <img src={logo} alt="GoBarber" />
 
-            <Form onSubmit={handleSubmit}>
+            <Form schema={schema} onSubmit={handleSubmit}>
                 <Input name="name" type="text" placeholder="Seu nome" />
                 <Input name="email" type="email" placeholder="Seu email" />
                 <Input
